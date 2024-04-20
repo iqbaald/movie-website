4fabf1b9fbb09d66cb9299b79dd6d869e0eeab60
Muhamad Iqbaal Dhoifulloh <iqbaaldhoifulloh@gmail.com>
2024-04-20 07:53:30 +0700

    add: axios library to fetch data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import "./App.css";
+import { getMovieList, searchMovie } from "./api";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    getMovieList().then((response) => {
+      setMovieList(response);
+    });
+  }, []);
+
+  const search = (q) => {
+    console.log({ q });
+  };
+
+  console.log({ movieList: movieList });
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Movie in Aja</h1>
+        <input
+          placeholder="Cari film..."
+          className="Movie-search"
+          onChange={({ target }) => {
+            search(target.value);
+          }}
+        />
         <div className="Movie-container">
           <div className="Movie-wrapper">
-            <div className="Movie-title"></div>
-            <img className="Movie-image" />
-            <div className="Movie-date"></div>
-            <div className="Movie-rate"></div>
+            <div className="Movie-title">Fallout</div>
+            <img className="Movie-image" src="" />
+            <div className="Movie-date">23-04-2024</div>
+            <div className="Movie-rate">8.9</div>
           </div>
         </div>
       </header>
--- src/api.js
+++ src/api.js
@@ -1,1 +1,13 @@
 import axios from "axios";
+
+export const getMovieList = async () => {
+  const baseUrl = process.env.REACT_APP_BASEURL;
+  const apiKey = process.env.REACT_APP_APIKEY;
+  const movie = await axios.get(`${baseUrl}/movie/popular?api_key=${apiKey}`);
+  return movie.data.results;
+};
+
+export const searchMovie = async () => {
+  const search = await axios.get(``);
+  return;
+};
